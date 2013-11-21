b56b67bbcf0be3ca6c61f056a60b8fdea6872ed6
songdz <910651267@qq.com>
2013-11-21 11:04:09 +0800

    UI is OK!

--- MyContacts_Test/gen/com/songdongze/mycontacts_test/R.java
+++ MyContacts_Test/gen/com/songdongze/mycontacts_test/R.java
@@ -24,33 +24,34 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001a;
-        public static final int button_Cancel=0x7f08000f;
-        public static final int button_OK=0x7f080010;
-        public static final int editText_Company=0x7f080002;
-        public static final int editText_CompanyPhone=0x7f080008;
-        public static final int editText_Email=0x7f08000b;
-        public static final int editText_Name=0x7f080001;
-        public static final int editText_PrivatePhone=0x7f080005;
-        public static final int editText_QQ=0x7f08000e;
-        public static final int imageButton_ContactPhoto=0x7f080000;
-        public static final int imageButton_DeleteContact=0x7f080019;
-        public static final int imageButton_EditContact=0x7f080018;
-        public static final int imageView_ContactPhoto=0x7f080011;
-        public static final int tableRow_CompanyPhone=0x7f080006;
-        public static final int tableRow_Email=0x7f080009;
-        public static final int tableRow_PrivatePhone=0x7f080003;
-        public static final int tableRow_QQ=0x7f08000c;
-        public static final int textView_Company=0x7f080013;
-        public static final int textView_CompanyPhone=0x7f080007;
-        public static final int textView_CompanyPhone2=0x7f080015;
-        public static final int textView_Email=0x7f08000a;
-        public static final int textView_Email2=0x7f080016;
-        public static final int textView_Name=0x7f080012;
-        public static final int textView_PrivatePhone=0x7f080004;
-        public static final int textView_PrivatePhone2=0x7f080014;
-        public static final int textView_QQ=0x7f08000d;
-        public static final int textView_QQ2=0x7f080017;
+        public static final int action_settings=0x7f08001b;
+        public static final int button_Cancel=0x7f080010;
+        public static final int button_OK=0x7f080011;
+        public static final int editText_Company=0x7f080003;
+        public static final int editText_CompanyPhone=0x7f080009;
+        public static final int editText_Email=0x7f08000c;
+        public static final int editText_Name=0x7f080002;
+        public static final int editText_PrivatePhone=0x7f080006;
+        public static final int editText_QQ=0x7f08000f;
+        public static final int empty_contacts=0x7f080000;
+        public static final int imageButton_ContactPhoto=0x7f080001;
+        public static final int imageButton_DeleteContact=0x7f08001a;
+        public static final int imageButton_EditContact=0x7f080019;
+        public static final int imageView_ContactPhoto=0x7f080012;
+        public static final int tableRow_CompanyPhone=0x7f080007;
+        public static final int tableRow_Email=0x7f08000a;
+        public static final int tableRow_PrivatePhone=0x7f080004;
+        public static final int tableRow_QQ=0x7f08000d;
+        public static final int textView_Company=0x7f080014;
+        public static final int textView_CompanyPhone=0x7f080008;
+        public static final int textView_CompanyPhone2=0x7f080016;
+        public static final int textView_Email=0x7f08000b;
+        public static final int textView_Email2=0x7f080017;
+        public static final int textView_Name=0x7f080013;
+        public static final int textView_PrivatePhone=0x7f080005;
+        public static final int textView_PrivatePhone2=0x7f080015;
+        public static final int textView_QQ=0x7f08000e;
+        public static final int textView_QQ2=0x7f080018;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -62,12 +63,18 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        public static final int add_contact=0x7f05000c;
         public static final int app_name=0x7f050000;
         public static final int cancel=0x7f05000a;
         public static final int companyPhone=0x7f050006;
         public static final int contactCompany=0x7f050004;
         public static final int contactName=0x7f050003;
+        public static final int contactedit=0x7f050010;
+        public static final int contactview=0x7f05000e;
+        public static final int deletecontact=0x7f05000f;
         public static final int eMail=0x7f050008;
+        public static final int empty_contacts=0x7f05000b;
+        public static final int exit=0x7f05000d;
         public static final int hello_world=0x7f050002;
         public static final int ok=0x7f050009;
         public static final int privatePhone=0x7f050005;
--- MyContacts_Test/src/com/songdongze/mycontacts_test/MainActivity.java
+++ MyContacts_Test/src/com/songdongze/mycontacts_test/MainActivity.java
@@ -1,22 +1,112 @@
 package com.songdongze.mycontacts_test;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends ListActivity {
+	
+	private ArrayList<String> item = getArrayList();
+	private static final int ADDCONTACT = Menu.FIRST;
+	private static final int EXIT = Menu.FIRST + 1;
+	private static final int DELETECONTACT = Menu.FIRST + 2;
+	
+	private ArrayList<String> getArrayList() {
+		ArrayList<String> item = new ArrayList<String>();
+		item.add("aaa");
+		item.add("bbb");
+		item.add("ccc");
+		return item;
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		getListView().setEmptyView(findViewById(R.id.empty_contacts));
+		this.registerForContextMenu(getListView());
+		setAdapter();
+		getListView().setTextFilterEnabled(true);
+	}
+	
+	private void setAdapter() {
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item));
 	}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, ADDCONTACT, 0, R.string.add_contact);
+		menu.add(1, EXIT, 0, R.string.exit);
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		switch(item.getItemId()) {
+			case ADDCONTACT: Intent intent = new Intent();
+				intent.setAction(Intent.ACTION_EDIT);
+				intent.addCategory("com.songdongze.mycontacts_test.category.ContactEdit");
+				startActivity(intent);
+				return true;
+			case EXIT:
+				this.finish();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		
+		Toast.makeText(getApplicationContext(), "position :" + position + "id :" + id, Toast.LENGTH_SHORT).show();
+		
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_VIEW);
+		intent.addCategory("com.songdongze.mycontacts_test.category.ContactView");
+		startActivity(intent);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo)menuInfo;
+		String name = (String)getListView().getAdapter().getItem(info.position);
+		menu.setHeaderTitle(R.string.deletecontact);
+		menu.add(0, DELETECONTACT, 0, name + "?");
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+
+		switch(item.getItemId()) {
+			case DELETECONTACT:
+				deleteRow(((AdapterContextMenuInfo)item.getMenuInfo()).position);
+				return true;
+		}
+		return false;
+	}
+
+	private void deleteRow(int position) {
+		item.remove(position);
+		((BaseAdapter)getListAdapter()).notifyDataSetChanged();
+		getListView().invalidate();
+	}
+	
+	
 
 }
